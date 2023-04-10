d98a6580af5dab2bbaefa9f8201f52adbb358edb
정승우 <sunjw0531@gmail.com>
2023-04-10 21:45:44 +0900

    Overloading
    The function which has many type of parameter or return type.

--- practice.ts
+++ practice.ts
@@ -1,5 +1,37 @@
-// const add = (a: number, b: number) => a + b;
-// make a function call signature
-type Add = (a: number, b: number) => number;
+type Add = {
+  (a: number, b: number): number;
+  (a: number, b: string): number;
+};
 
-const add: Add = (a, b) => a + b;
+const add: Add = (a, b) => {
+  if (typeof b === 'string') return a;
+  return a + b;
+};
+
+type Config = {
+  path: string;
+  state: object;
+};
+type Push = {
+  (path: string): void;
+  (obj: Config): void;
+};
+const push: Push = (config) => {
+  if (typeof config === 'string') console.log(config);
+  else {
+    console.log(config.path);
+  }
+};
+
+// different parameter count
+type Add1 = {
+  (a: number, b: number): number;
+  (a: number, b: number, c: number): number;
+};
+const add1: Add1 = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
+};
+
+add1(1, 2);
+add1(1, 2, 3);

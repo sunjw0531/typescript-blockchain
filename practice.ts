04ca21d5ece4713a135aeae151f05482fdf016a1
정승우 <sunjw0531@gmail.com>
2023-04-16 22:07:11 +0900

    recap

---
+++ practice.ts
@@ -0,0 +1,29 @@
+type Words = {
+  [key: string]: string;
+};
+
+class Dict {
+  private words: Words;
+  constructor() {
+    this.words = {};
+  }
+  add(word: Word) {
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
+    }
+  }
+  def(term: string) {
+    return this.words[term];
+  }
+}
+
+class Word {
+  constructor(public term: string, public def: string) {}
+}
+
+const food = new Word('kimchi', '김치');
+
+const dict = new Dict();
+
+dict.add(food);
+dict.def('kimchi');

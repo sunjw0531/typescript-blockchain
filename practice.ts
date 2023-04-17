d3da5de8117ce5ceac0ff7418ec3bec4ad281455
정승우 <sunjw0531@gmail.com>
2023-04-17 21:25:47 +0900

    recap one more time

--- practice.ts
+++ practice.ts
@@ -1,6 +1,8 @@
+// object type definition
 type Words = {
   [key: string]: string;
 };
+// let dict : Words ={'potato' : 'food', 'pizza' : 'food'}
 
 class Dict {
   private words: Words;
@@ -15,6 +17,11 @@ class Dict {
   def(term: string) {
     return this.words[term];
   }
+  update(newWord: Word) {
+    return this.words[newWord.term]
+      ? (this.words[newWord.term] = newWord.def)
+      : console.log('No word');
+  }
 }
 
 class Word {
@@ -26,4 +33,10 @@ const food = new Word('kimchi', '김치');
 const dict = new Dict();
 
 dict.add(food);
+// words = {'kimchi' : '김치'}
+
 dict.def('kimchi');
+// 김치
+
+const newFoodDef = new Word('kimchi', '새로운 김치');
+dict.update(newFoodDef);

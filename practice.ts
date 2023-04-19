99a4e9e9cc670190838dab9fa70a75067b44b198
정승우 <sunjw0531@gmail.com>
2023-04-19 21:38:22 +0900

    Interfaces 2

--- practice.ts
+++ practice.ts
@@ -1,85 +1,59 @@
-// type shape of an Object
-type Player = {
-  nickname: string;
-  healthBar: number;
-};
+// abstract class User {
+//   constructor(protected firstName: string, protected lastName: string) {}
+//   abstract sayHi(name: string): string;
+//   abstract fullName(): string;
+// }
 
-const man: Player = {
-  nickname: 'man',
-  healthBar: 10,
-};
+// class Player extends User {
+//   fullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+//   sayHi(name: string) {
+//     return `Hello ${name}. My name is ${this.fullName()}`;
+//   }
+// }
 
-// type
-type Food = string;
-
-const soup: Food = 'delicious';
-
-// type alias(대체명)
-type Nickname = string;
-type Health = number;
-type Friends = Array<string>;
-type Player1 = {
-  nickname: Nickname;
-  healthBar: Health;
-};
-
-// type with specific option (concrete type)
-type Team = 'red' | 'blue' | 'yellow';
-type Health2 = 1 | 5 | 10;
-type Player2 = {
-  nickname: string;
-  team: Team;
-  health: Health2;
-};
-const man2: Player2 = {
-  nickname: 'man2',
-  team: 'yellow',
-  health: 1,
-};
-
-//////////////////////////////////////////
-// interface only specify the shape of Object
-interface Player3 {
-  nickname: string;
-  team: Team;
-  health: Health2;
-}
-
-const man3: Player3 = {
-  nickname: 'man2',
-  team: 'yellow',
-  health: 1,
-};
-
-// interface extends
+// in javascript, there is no interface code
+// but abstract class, there is a class code.
+// so interface can reduce the code size
 interface User {
-  readonly name: string;
-}
-interface Player4 extends User {}
-const man4: Player4 = {
-  name: 'man4',
-};
-// err
-man4.name = 'asdf';
-
-// if it was a type?
-type User1 = {
-  name: string;
-};
-type Player5 = User1 & {};
-
-// interface can stack the different properties in same interfaces
-interface User6 {
-  name: string;
-}
-interface User6 {
+  firstName: string;
   lastName: string;
+  sayHi(name: string): string;
+  fullName(): string;
 }
-interface User6 {
-  health: Number;
+
+interface Human {
+  health: number;
 }
-const newUser: User6 = {
-  name: 'man',
-  lastName: 'hey',
-  health: 3,
-};
+
+class Player implements User, Human {
+  constructor(
+    // cannot use private and protected while using interface
+    public firstName: string,
+    public lastName: string,
+    public health: number
+  ) {}
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  sayHi(name: string) {
+    return `Hello ${name}. My name is ${this.fullName()}`;
+  }
+}
+
+function makeUser(user: User): User {
+  return {
+    firstName: 'man',
+    lastName: 'last',
+    fullName: () => 'asdf',
+    sayHi: (name) => 'adsfasdkjl',
+  };
+}
+
+makeUser({
+  firstName: 'man',
+  lastName: 'last',
+  fullName: () => 'asdf',
+  sayHi: (name) => 'adsfasdkjl',
+});

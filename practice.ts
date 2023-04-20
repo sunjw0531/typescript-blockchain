b8038158788fddfe5fcc3b2f01d0dafd0ede3459
정승우 <sunjw0531@gmail.com>
2023-04-20 21:02:42 +0900

    Recap

--- practice.ts
+++ practice.ts
@@ -1,59 +1,15 @@
-// abstract class User {
-//   constructor(protected firstName: string, protected lastName: string) {}
-//   abstract sayHi(name: string): string;
-//   abstract fullName(): string;
-// }
-
-// class Player extends User {
-//   fullName() {
-//     return `${this.firstName} ${this.lastName}`;
-//   }
-//   sayHi(name: string) {
-//     return `Hello ${name}. My name is ${this.fullName()}`;
-//   }
-// }
-
-// in javascript, there is no interface code
-// but abstract class, there is a class code.
-// so interface can reduce the code size
-interface User {
+type PlayerA = {
   firstName: string;
-  lastName: string;
-  sayHi(name: string): string;
-  fullName(): string;
+};
+
+interface PlayerB {
+  firstName: string;
 }
 
-interface Human {
-  health: number;
+class User implements PlayerA {
+  constructor(public firstName: string) {}
 }
 
-class Player implements User, Human {
-  constructor(
-    // cannot use private and protected while using interface
-    public firstName: string,
-    public lastName: string,
-    public health: number
-  ) {}
-  fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  sayHi(name: string) {
-    return `Hello ${name}. My name is ${this.fullName()}`;
-  }
+class User1 implements PlayerB {
+  constructor(public firstName: string) {}
 }
-
-function makeUser(user: User): User {
-  return {
-    firstName: 'man',
-    lastName: 'last',
-    fullName: () => 'asdf',
-    sayHi: (name) => 'adsfasdkjl',
-  };
-}
-
-makeUser({
-  firstName: 'man',
-  lastName: 'last',
-  fullName: () => 'asdf',
-  sayHi: (name) => 'adsfasdkjl',
-});

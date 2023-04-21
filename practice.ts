627fc31b503b555b2c90a3c54444ad05beb9f567
sunjw0531 <sunjw0531@naver.com>
2023-04-21 22:23:30 +0900

    Recap one more

--- practice.ts
+++ practice.ts
@@ -2,9 +2,30 @@ type PlayerA = {
   firstName: string;
 };
 
+type PlayerAA = PlayerA & {
+  lastName: string;
+};
+
+const playerA: PlayerAA = {
+  firstName: "man",
+  lastName: "asdf",
+};
+
 interface PlayerB {
   firstName: string;
 }
+
+interface PlayerBB extends PlayerB {
+  lastName: string;
+}
+interface PlayerBB {
+  a: string;
+}
+const playerB: PlayerBB = {
+  firstName: "men",
+  lastName: "asdfqwe",
+  a: "a",
+};
 
 class User implements PlayerA {
   constructor(public firstName: string) {}

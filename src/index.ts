7836ddc72fbfa4a8ab44e681cd46284022343b1e
정승우 <sunjw0531@gmail.com>
2023-04-28 21:45:01 +0900

    practice

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-import { init, exit } from './myPackage.js';
+import { init, exit, example } from './myPackage.js';
+
+init({ debug: true, url: 'asdf' });
+exit(1);
+example({ num: 1, str: 'a' }, true);
--- src/myPackage.js
+++ src/myPackage.js
@@ -21,3 +21,15 @@ export function init(config) {
 export function exit(code) {
   return code + 1;
 }
+
+/**
+ *
+ * @param {object} ex1
+ * @param {number} ex1.num
+ * @param {string} ex1.str
+ * @param {boolean} ex2
+ * @returns {object}
+ */
+export function example(ex1, ex2) {
+  return { ex1, ex2 };
+}
